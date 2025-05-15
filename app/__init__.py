ca8a97b89ee2a511b160db4b68dd4c7ad7671f9f
Eylul Kadioglu <70960300+eylulkadioglu@users.noreply.github.com>
2025-05-15 14:16:01 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'your databa URI'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'your database URI'
     db.init_app(app)
 
     with app.app_context():
